b8d57e706afda19ff15b8b26db46b732df19f108
Marco Silva <marcowilliamdf@gmail.com>
2016-03-27 20:33:19 -0300

    installing and cofiguring spring secutiry plugin

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,6 +1,16 @@
+import formar.*
+
 class BootStrap {
 
     def init = { servletContext ->
+    
+    	def adminRole = Role.findOrSaveWhere(authority: 'ROLE_ADMIN')
+    	def user = User.findOrSaveWhere(username: 'marcowilliamdf@gmail.com', password: 'admin')
+
+    	if( !user.authorities.contains(adminRole)){
+
+    		UserRole.create(user,adminRole,true)
+    	}
     }
     def destroy = {
     }
